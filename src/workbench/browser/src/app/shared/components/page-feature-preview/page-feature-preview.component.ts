c1c838e9a113843ba9aea862b313ebca2d6c4700
kungfuboy <17kungfuboy@gmail.com>
2022-06-01 11:54:56 +0800

    feat: new tip

--- src/workbench/browser/src/app/shared/components/page-feature-preview/page-feature-preview.component.ts
+++ src/workbench/browser/src/app/shared/components/page-feature-preview/page-feature-preview.component.ts
@@ -3,13 +3,34 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'eo-page-feature-preview',
   templateUrl: './page-feature-preview.component.html',
-  styleUrls: ['./page-feature-preview.component.scss']
+  styleUrls: ['./page-feature-preview.component.scss'],
 })
 export class PageFeaturePreviewComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor() {}
+  version = '1.0.3';
+  downloadList = [
+    {
+      id: 'win',
+      name: 'Windows 客户端',
+      icon: 'windows',
+      keyword: 'Setup',
+      suffix: 'exe',
+      link: '',
+    },
+    {
+      id: 'mac',
+      name: 'macOS(Intel) 客户端',
+      icon: 'apple',
+      suffix: 'dmg',
+      link: '',
+    },
+    {
+      id: 'mac',
+      name: 'macOS(M1) 客户端',
+      icon: 'apple',
+      suffix: 'arm64.dmg',
+      link: '',
+    },
+  ];
+  ngOnInit(): void {}
 }
